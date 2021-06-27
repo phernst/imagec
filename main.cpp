db28b5c1505150bbfd6877ef8846825d275fd6e4
Philipp Ernst <phil23940@yahoo.de>
2021-06-27 19:45:19 +0200

    add libtorch

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include <QtWidgets>
 
+#undef slots
+#include <torch/torch.h>
+#define slots Q_SLOTS
+
+#include <iostream>
+
 #include "mainwindow.h"
 
 int main(int argc, char** argv)
 {
     auto a = QApplication{argc, argv};
     auto mainWindow = MainWindow{};
+
+    using namespace torch::indexing;
+
+    const auto t = torch::rand({3, 4});
+    std::cout << t.index({Slice(1, 3), Slice(1, 3)}) << std::endl;
 
     mainWindow.show();
     return a.exec();
